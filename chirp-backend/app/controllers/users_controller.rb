983acd3d8a49efbdd305fc999c60cd3380a31df2
LilyCole <Lilia.Cole@gmail.com>
2016-11-16 11:35:08 -0800

    adds controller method stubs

--- chirp-backend/app/controllers/users_controller.rb
+++ chirp-backend/app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :image, :password)
+  end
 end
