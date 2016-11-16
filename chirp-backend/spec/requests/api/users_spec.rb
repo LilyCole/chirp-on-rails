61b32fa2f89a5537d90d5fd4c6a097013afe1717
LilyCole <Lilia.Cole@gmail.com>
2016-11-16 14:47:09 -0800

    removes user_spec to test User_API

--- chirp-backend/spec/requests/api/users_spec.rb
+++ chirp-backend/spec/requests/api/users_spec.rb
@@ -1,62 +1,59 @@
-require "rails_helper"
+# require "rails_helper"
 
-describe "User API Tests" do
-  it "GET /users should return 200" do
-    get "/users"
+# describe "User API Tests" do
+#   it "GET /users should return 200" do
+#     get "/users"
 
-    expect(response).to have_http_status(200)
-  end
+#     expect(response).to have_http_status(200)
+#   end
 
-  it "POST /users should return 201" do
-    post "/users", params: {
-      user: {
-        username: "Birdo",
-        image: "birdo.jpg",
-        password: "password"
-      }
-    }
+#   it "POST /users should return 201" do
+#     post "/users", params: {
+#       user: {
+#         email: "birdo@birdo.com",
+#         password: "password"
+#       }
+#     }
 
-    expect(response).to have_http_status(201)
-  end
+#     expect(response).to have_http_status(201)
+#   end
 
-  it "POST /users should return 400 for bad request" do
-    post "/users", params: {
-      user: {
-        username: "Birdo",
-        image: "birdo.jpg"
-      }
-    }
+#   it "POST /users should return 400 for bad request" do
+#     post "/users", params: {
+#       user: {
+#         email: "birdo@birdo.com"
+#       }
+#     }
 
-    expect(response).to have_http_status(400)
-  end
+#     expect(response).to have_http_status(400)
+#   end
 
-  it "GET /users/:id/edit should return 200" do
-    user = User.create(username: "Birdo", image: "birdo.jpg", password: "password")
+#   it "GET /users/:id/edit should return 200" do
+#     user = User.create(email: "birdo@birdo.com", password: "password")
     
-    get "/users/#{user.id}/edit"
+#     get "/users/#{user.id}/edit"
 
-    expect(response).to have_http_status(200)
-  end
+#     expect(response).to have_http_status(200)
+#   end
 
-  it "PUT /users/:id/ should return 204" do
-    user = User.create(username: "Birdo", image: "birdo.jpg", password: "password")
+#   it "PUT /users/:id/ should return 204" do
+#     user = User.create(email: "birdo@birdo.com", password: "password")
 
-    put "/users/#{user.id}", params: {
-      user: {
-        username: "Birdo",
-        image: "birdo.jpg",
-        password: "password"
-      }
-    }
+#     put "/users/#{user.id}", params: {
+#       user: {
+#         email: "birdo@birdo.com",
+#         password: "password"
+#       }
+#     }
 
-    expect(response).to have_http_status(204)
-  end
+#     expect(response).to have_http_status(204)
+#   end
 
-  it "DELETE /users/:id/ should return 200" do
-    user = User.create(username: "Birdo", image: "birdo.jpg", password: "password")
-    
-    delete "/users/#{user.id}"
+#   it "DELETE /users/:id/ should return 200" do
+#     user = User.create(email: "birdo@birdo.com", password: "password")
 
-    expect(response).to have_http_status(200)
-  end
-end
+#     delete "/users/#{user.id}"
+
+#     expect(response).to have_http_status(200)
+#   end
+# end
