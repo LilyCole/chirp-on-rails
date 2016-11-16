c913bc17f8c0cdfe660f59a6207bddc518198f58
LilyCole <Lilia.Cole@gmail.com>
2016-11-16 11:39:00 -0800

    adds routes for chirps and controller method stubs

--- chirp-backend/app/controllers/chirps_controller.rb
+++ chirp-backend/app/controllers/chirps_controller.rb
@@ -1,2 +1,27 @@
 class ChirpsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def chirp_params
+    params.require(:chirp).permit(:user, :content)
+  end
+
 end
